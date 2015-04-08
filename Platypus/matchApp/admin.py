12063f54da047a1ef69aeceb313517848bae354d
leokim89 <amos.kim@colorado.edu>
2015-04-08 16:20:36 -0600

    reset Platypus folder

--- Platypus/matchApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from matchApp.models import Course, Section, Student
-
-admin.site.register(Course)
-admin.site.register(Section)
-admin.site.register(Student)
--- Platypus/matchApp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-from matchApp import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^home/$', views.home, name='home'))
