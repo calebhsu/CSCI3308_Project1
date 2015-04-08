dfce1a6e6932ec07c17406da498c8acc026369cb
Caleb Hsu <chialo.hsu@colorado.edu>
2015-04-07 22:27:12 -0600

    Fixed broken database

--- Platypus/matchApp/admin.py
+++ Platypus/matchApp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from matchApp.models import Student, Course
+from matchApp.models import Course, Section, Student
 
-admin.site.register(Student)
 admin.site.register(Course)
+admin.site.register(Section)
+admin.site.register(Student)
--- Platypus/matchApp/models.py
+++ Platypus/matchApp/models.py
@@ -2,21 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 '''
-class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-
-    def __unicode__(self):  #For Python 2, use __str__ on Python 3
-        return self.name
-
-class Page(models.Model):
-    category = models.ForeignKey(Category)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
-
-    def __unicode__(self):      #For Python 2, use __str__ on Python 3
-        return self.title
-        '''
 class Student(models.Model):
 		user = models.ForeignKey(User)
 		courses = models.CharField(max_length = 4, default = "")
@@ -32,6 +17,39 @@ class Course(models.Model):
 	
 	def __unicode__(self):
 		return self.number
-		
+'''
 
-	
+class Course(models.Model):
+	title = models.CharField(max_length=64, default="")
+	dept_id = models.CharField(max_length=4, default="")
+	course_number = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.title	
+
+class Section(models.Model):
+	class_id = models.IntegerField(default=0)
+	course_title = models.ForeignKey(Course, null=True)
+	#course_title = models.CharField(max_length=64, default="")
+	section_number = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.class_id	
+
+class Student(models.Model):
+	student_id = models.IntegerField(default=0)
+	first_name = models.CharField(max_length=64, default="")
+	last_name = models.CharField(max_length=64, default="")
+
+	user_id = models.CharField(max_length=64, default="")
+	password = models.CharField(max_length=64, default="")
+
+	email_address = models.CharField(max_length=128, default="")
+
+	course_list = models.CharField(max_length=1024, default="")
+
+	#view_url = models.URLField()
+	#pic_url = models.URLField()
+
+	def __unicode__(self):
+		return self.student_id
