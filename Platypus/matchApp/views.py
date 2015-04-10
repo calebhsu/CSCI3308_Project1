3ee6069f1a76afabea173bdcaeae295a169c0cb0
Caleb Hsu <chialo.hsu@colorado.edu>
2015-04-10 11:33:09 -0600

    Fixed floating buttons, edited class page

--- Platypus/matchApp/views.py
+++ Platypus/matchApp/views.py
@@ -18,15 +18,13 @@ def index(request):
     return render_to_response('matchApp/login.html', context_dict, context)
 
 def home(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
     context = RequestContext(request)
-
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     context_dict = {}
 
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
     return render_to_response('matchApp/home.html', context_dict, context)
+
+def classpage(request):
+    context = RequestContext(request)
+    context_dict = {}
+
+    return render_to_response('matchApp/classpage.html', context_dict, context)
