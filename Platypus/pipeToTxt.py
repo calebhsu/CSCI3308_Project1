95e8846e407b497ed1d2db5ccf86e9901f24279b
leokim89 <amos.kim@colorado.edu>
2015-04-09 23:16:50 -0600

    pipeToTxt.py fixed; now queries all required fields correctly

--- Platypus/pipeToTxt.py
+++ Platypus/pipeToTxt.py
@@ -10,11 +10,22 @@ from matchApp.models import Course, Section, Student, User
 import sys
 
 orig_stdout = sys.stdout
-f = file('student.txt', 'w')
-sys.stdout = f
+student_file = file('students.txt', 'w')
+sys.stdout = student_file
 
-for i in Student.objects.raw('SELECT id, course_list, u FROM matchApp_student'):
-    print i
+for i in Student.objects.all():
+	print i
+	print i.course_list
 
 sys.stdout = orig_stdout
-f.close()
+student_file.close()
+
+orig_stdout = sys.stdout
+section_file = file('sections.txt', 'w')
+sys.stdout = section_file
+
+for i in Section.objects.all():
+	print i
+
+sys.stdout = orig_stdout
+section_file.close()
