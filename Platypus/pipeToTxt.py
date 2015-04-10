c691739ea8719f5dcb7406e0f8deba32cf03a403
leokim89 <amos.kim@colorado.edu>
2015-04-10 00:06:23 -0600

    implemented cleaner query/ouput strategy for pipeToText.py; commented and improved readability

--- Platypus/pipeToTxt.py
+++ Platypus/pipeToTxt.py
@@ -1,31 +1,34 @@
+##############################################################################
+# Always run this block first so environmental variables are properly loaded #
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Platypus.settings')
+##############################################################################
 
+import sys
 import django
+from matchApp.models import Course, Section, Student, User
+
+
 django.setup()
 
-from matchApp.models import Course, Section, Student, User
-import sys
-
+#Retrieve current working directory so files can be saved to a nested folder
 current_directory = os.getcwd()
 
-orig_stdout = sys.stdout
-student_file = file(current_directory+'/matchApp/matchingAlgorithm/'+'students.txt', 'w')
-sys.stdout = student_file
+#Redirect standard output to new students.txt file in specified nested directory (/matchApp/matchingAlgorithm)
+sys.stdout = open(current_directory+'/matchApp/matchingAlgorithm/'+'students.txt', 'w')
 
+#Query all rows in Student table; Output return value for each object and its course_list attribute
 for i in Student.objects.all():
 	print i
 	print i.course_list
 
-sys.stdout = orig_stdout
-student_file.close()
+sys.stdout.close()
 
-orig_stdout = sys.stdout
-section_file = file(current_directory+'/matchApp/matchingAlgorithm/'+'sections.txt', 'w')
-sys.stdout = section_file
+#Redirect standard output to new sections.txt file in specified nested directory (/matchApp/matchingAlgorithm)
+sys.stdout = file(current_directory+'/matchApp/matchingAlgorithm/'+'sections.txt', 'w')
 
+#Query all rows in Section table; Output return value for each object
 for i in Section.objects.all():
 	print i
 
-sys.stdout = orig_stdout
-section_file.close()
+sys.stdout.close()
