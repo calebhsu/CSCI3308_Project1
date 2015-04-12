bd55ac74c03bd6b6359ef229965e824a1614bf31
leokim89 <amos.kim@colorado.edu>
2015-04-12 17:44:32 -0600

    split returnCourseList.py into returnCourseList.py and returnSectionsList.py for easier access by HTML coders

--- Platypus/matchApp/matchingAlgorithm/returnSectionsList.py
+++ Platypus/matchApp/matchingAlgorithm/returnSectionsList.py
@@ -20,26 +20,14 @@ sys.path.append(directory)
 from matchApp.models import Course, Section, Student, User
 django.setup()
 
-def returnCourseList(student_id):
+def returnSectionsList(student_id):
 	queried_student = Student.objects.get(user__username=student_id) #use "__" to access foreign key attributes
 	course_array = str(queried_student.course_list).split(',')
-
-	return_array = []
-	for course in course_array:
-		queried_course = Section.objects.get(class_id=course)
-		dept_id = str(queried_course.course_title.dept_id)
-		course_number = str(queried_course.course_title.course_number)
-		course_title = str(queried_course.course_title)
-		return_array.append(dept_id+course_number+"\n"+course_title+"\n")
-
-	return return_array
-
-
+	return course_array
 
 
 def main():
-	for course in returnCourseList(900000001):
-		print course
+	print returnSectionsList(900000001)
 
 if __name__ == '__main__':
 	main()
