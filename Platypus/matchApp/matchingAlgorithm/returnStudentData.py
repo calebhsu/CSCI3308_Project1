e1dd16893633e24cb9e4349609ae9be44f43534c
leokim89 <amos.kim@colorado.edu>
2015-04-12 18:24:44 -0600

    returns relevant information about student; easy output for HTML team

---
+++ Platypus/matchApp/matchingAlgorithm/returnStudentData.py
@@ -0,0 +1,47 @@
+##############################################################################
+# Always run this block first so environmental variables are properly loaded #
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Platypus.settings')
+##############################################################################
+
+import sys
+import django
+
+##############################################################################
+# Append parent of the parent directory for cwd to system path               #
+os.getcwd()
+os.chdir('..')
+os.chdir('..')
+directory = os.getcwd()
+
+sys.path.append(directory)
+##############################################################################
+
+from matchApp.models import Course, Section, Student, User
+django.setup()
+
+##############################################################################
+# The contents of this function will be highly variable, and must be tailored
+# to the actual content needed by the HTML coders. For now, include all the
+# functionality that might be needed, and comment out. 
+# Uncomment required fields later as needed by the HTML/front-end team.
+# 
+##############################################################################
+
+def returnStudentData(student_id):
+	queried_student = Student.objects.get(user__username=student_id) #use "__" to access foreign key attributes
+	username = queried_student.user.username
+	name = str(queried_student.user.first_name)+" "+str(queried_student.user.last_name)
+	email = queried_student.user.email
+
+	print "Username:\t"+username
+	print "Name:\t\t"+name
+	print "Email:\t\t"+email+"\n"
+
+	return queried_student
+
+# def main():
+# 	returnStudentData(900000001)
+
+# if __name__ == '__main__':
+# 	main()
