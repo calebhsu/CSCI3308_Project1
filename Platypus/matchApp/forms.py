dd9bd3418439f5c1bd946a379930b631d542c276
Caleb Hsu <chialo.hsu@colorado.edu>
2015-04-14 19:57:11 -0600

    Added registration page - CH, ML

--- Platypus/matchApp/forms.py
+++ Platypus/matchApp/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from matchApp.models import Student, Course, Section 
+from matchApp.models import Student, Course, Section
+from django.contrib.auth.models import User
+from django.db import models
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
